24ef813804d7e85bbc5c803eb5726789dad35c74
Rafael <RafaelB24@yahoo.com>
2020-02-12 15:46:22 -0800

    Change Signup to Register

--- stock-portfolio-frontend/src/App.js
+++ stock-portfolio-frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Home} from './components/Home';
-import Signup from './components/Signup';
+import Register from './components/Register';
 
 function App() {
   return (
     <div >
       <Home /> 
-      <Signup />
+      <Register />
     </div>
   );
 }
