5354032dec05b1632738d8d50ac9b7d9dc9a53e3
Rafael <RafaelB24@yahoo.com>
2020-02-12 15:45:35 -0800

    Change Signup to class and add state, add handleOnChange to Signup

--- stock-portfolio-frontend/src/components/Signup.js
+++ stock-portfolio-frontend/src/components/Signup.js
@@ -1,24 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export const Signup = () => {
-    return (
-        <div className="container  p-3" id="login">
-             <form>
-                <div class="form-group">
-                    <label HTMLfor="email">Email address</label>
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" />
+ class Signup extends Component {
+     constructor(props) {
+         super(props)
+         this.state = {name: '', email: '', password: ''}
+     }
+
+     handleOnChange = event => {
+        this.setState({
+          [event.target.name]: event.target.value,
+        });
+      }
+    render() {
+        return (
+            <div className="container  p-3" id="login">
+                <h3 className="text-center">Register</h3>
+                <form>
+                     <div class="form-group">
+                        <label HTMLfor="name">Name</label>
+                        <input type="name" class="form-control"  name="name" onChange={this.handleOnChange} />
+                    </div>
+
+                    <div class="form-group">
+                        <label HTMLfor="email">Email address</label>
+                        <input type="email" class="form-control"  name="email" onChange={this.handleOnChange} />
+                    </div>
+
+                    <div class="form-group">
+                        <label HTMLfor="password">Password</label>
+                        <input type="password" class="form-control" name="password" onChange={this.handleOnChange}/>
+                    </div>
                     
-                </div>
-                <div class="form-group">
-                    <label HTMLfor="password">Password</label>
-                    <input type="password" class="form-control" id="password" />
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
-       
-       
-    )
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Signup
