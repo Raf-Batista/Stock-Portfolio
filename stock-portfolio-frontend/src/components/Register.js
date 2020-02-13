1eae8133047fea89feea5e6b7611924c64047f1a
Rafael <RafaelB24@yahoo.com>
2020-02-12 17:21:28 -0800

    Add createUser function to create user via POST req, add onClick event listener to submit button

--- stock-portfolio-frontend/src/components/Register.js
+++ stock-portfolio-frontend/src/components/Register.js
@@ -11,27 +11,53 @@ import React, { Component } from 'react'
           [event.target.name]: event.target.value,
         });
       }
+
+      createUser = async () => {
+        const URL = "http://localhost:3000/users"; 
+        const settings = {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            user: this.state
+        };
+        try {
+            const fetchResponse = await fetch(URL, settings);
+            const data = await fetchResponse.json();
+            console.log(data)
+        } catch(error) {
+            console.log(error)
+        }
+   
+      }
+
+      handleOnClick = event => {
+        event.preventDefault()
+        this.createUser();
+      }
+
     render() {
         return (
             <div className="container  p-3" id="login">
                 <h3 className="text-center">Register</h3>
                 <form>
-                     <div class="form-group">
-                        <label HTMLfor="name">Name</label>
-                        <input type="name" class="form-control"  name="name" onChange={this.handleOnChange} />
+                     <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="name" className="form-control"  name="name" onChange={this.handleOnChange} />
                     </div>
 
-                    <div class="form-group">
-                        <label HTMLfor="email">Email address</label>
-                        <input type="email" class="form-control"  name="email" onChange={this.handleOnChange} />
+                    <div className="form-group">
+                        <label htmlFor="email">Email address</label>
+                        <input type="email" className="form-control"  name="email" onChange={this.handleOnChange} />
                     </div>
 
-                    <div class="form-group">
-                        <label HTMLfor="password">Password</label>
-                        <input type="password" class="form-control" name="password" onChange={this.handleOnChange}/>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="form-control" name="password" onChange={this.handleOnChange}/>
                     </div>
                     
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary" onClick={this.handleOnClick}>Submit</button>
                 </form>
             </div>
         )
