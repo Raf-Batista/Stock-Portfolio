e60559202fc229dd7e6e5963f3a3eda13cfac0e1
Rafael <RafaelB24@yahoo.com>
2020-02-13 09:01:01 -0800

    Create Navbar.js, only renders navbar if user is logged in

--- stock-portfolio-frontend/src/App.js
+++ stock-portfolio-frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import {Home} from './components/Home';
+import { Home } from './components/Home';
+import { Navbar } from './components/Navbar';
 import Register from './components/Register';
 
 function App() {
   return (
     <div >
       <Home /> 
+      <Navbar />
       <Register />
     </div>
   );
---
+++ stock-portfolio-frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+export const Navbar = () => {
+    const nav = 
+    <div className="container">
+        <div className="float-right">
+        <NavLink className="mx-2" exact to='/portfolio'>Portfolio</NavLink>
+        <NavLink exact to='/transactions'>Transactions</NavLink>
+        </div>      
+    </div>
+  
+    return (
+       localStorage.token ? nav : null // Render navbar only if user is logged in
+    )
+}
