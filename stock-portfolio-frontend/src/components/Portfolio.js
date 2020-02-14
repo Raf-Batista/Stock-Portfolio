d31c90f8ed1b0628ed59af5574043f749d55c796
Rafael <RafaelB24@yahoo.com>
2020-02-14 07:44:27 -0800

    Maps through state of stocks and renders a Portfolio container with stock info

--- stock-portfolio-frontend/src/components/Portfolio.js
+++ stock-portfolio-frontend/src/components/Portfolio.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react'
 class Portfolio extends Component {
     render() {
         return (
-            <div>
-                Portfolio
+            <div className="my-3">
+                <div>
+                    {this.props.symbol} - {this.props.shares} Shares ${this.props.value}
+                </div>
+                <hr className="w-25"></hr>
             </div>
         )
     }
--- stock-portfolio-frontend/src/containers/PortfolioContainer.js
+++ stock-portfolio-frontend/src/containers/PortfolioContainer.js
@@ -1,10 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Portfolio from '../components/Portfolio';
 
 class PortfolioContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {stocks : [{symbol: 'test', shares: 2, value: 234.24}, {symbol: 'test2', shares: 2, value: 234.24}], balance: 5000};
+    }
+
     render() {
         return (
-            <div>
-                PortfolioContainer
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col">
+                    <h2 className="my-4">Portfolio (${this.state.balance})</h2>
+                    {this.state.stocks.map((stock) => (
+                        <Portfolio symbol={stock.symbol} shares={stock.shares} value={stock.value}/>
+                    ))}
+                    </div>
+                    <div className="col">asdfasdf</div>
+                </div>
+              
             </div>
         )
     }
