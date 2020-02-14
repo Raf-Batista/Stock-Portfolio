342485716b1d28592552eb530ffb465eda6dddb6
Rafael <RafaelB24@yahoo.com>
2020-02-14 05:15:43 -0800

    Create login method and add email and password state to form

--- stock-portfolio-frontend/src/components/Login.js
+++ stock-portfolio-frontend/src/components/Login.js
@@ -3,7 +3,31 @@ import React, { Component } from 'react'
  class Login extends Component {
     constructor(props) {
         super(props)
-        this.state = {name: '', email: '', password: ''}
+        this.state = {email: '', password: ''}
+    }
+
+    login = async () => {
+        const URL = "http://localhost:3000/login"; 
+        try {
+            const fetchResponse = await fetch(URL, {
+                method: 'POST',
+                body: JSON.stringify({user: this.state}),
+                headers:{
+                  'Content-Type': 'application/json'
+                }
+            });
+            const data = await fetchResponse.json();
+            if(data.token) localStorage.setItem('userData', JSON.stringify(data));
+            this.setState({email: '', password: ''})
+            this.props.history.push('/portfolio');
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    handleOnClick = (event) => {
+        event.preventDefault()
+        this.login();
     }
     
     render() {
@@ -13,12 +37,12 @@ import React, { Component } from 'react'
                 <form>
                     <div className="form-group">
                         <label htmlFor="email">Email address</label>
-                        <input type="email" className="form-control"  name="email" onChange={this.handleOnChange} />
+                        <input type="email" className="form-control"  name="email" onChange={this.handleOnChange} value={this.state,email}/>
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" onChange={this.handleOnChange}/>
+                        <input type="password" className="form-control" name="password" onChange={this.handleOnChange} value={this.state.password}/>
                     </div>
                     
                     <button type="submit" className="btn btn-primary mb-3" onClick={this.handleOnClick}>Login</button>
