5f60d68883a294bd4b8295915d7862af6ae9f5b9
Rafael <RafaelB24@yahoo.com>
2020-02-16 12:55:37 -0800

    Wrap div in a form tag, move onClick to form as onSubmit, rename handleOnChange to handleChange

--- stock-portfolio-frontend/src/components/StocksForm.js
+++ stock-portfolio-frontend/src/components/StocksForm.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 class StocksForm extends Component {
     constructor(props) {
         super(props)
-        this.state = {ticker: '', qty: 0}
+        this.state = {ticker: '', qty: ''}
     }
 
-    handleOnChange = (event) => {
+    handleChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value
         })
@@ -34,7 +34,7 @@ class StocksForm extends Component {
                 }
             })
 
-            this.setState({ticker: '', qty: 0});
+            this.setState({ticker: '', qty: ''});
             this.props.fetchUserStocks(); // Passed from parent, fetch the user's stock and update state to reflect newly bought stock
 
         } catch (error) {
@@ -42,7 +42,7 @@ class StocksForm extends Component {
         }
     }
 
-    handleOnClick = (event) => {
+    handleSubmit = (event) => {
         event.preventDefault();
         this.fetchStocks();
     }
@@ -51,13 +51,19 @@ class StocksForm extends Component {
         return (
             <div className="container">
                 <h2 className="mt-4 ml-5">Cash - $ {this.props.balance}</h2>
-                <div class="form-group my-4 ml-5 w-50">
-                    <input name="ticker" type="text" class="form-control" placeholder="Ticker" value={this.state.ticker} onChange={this.handleOnChange}/>
-                </div>
-                <div class="form-group ml-5 w-50">
-                    <input name="qty" type="text" class="form-control" placeholder="Qty" value={this.state.qty} onChange={this.handleOnChange}/>
-                </div>
-                <button type="submit" class="btn btn-primary" onClick={this.handleOnClick}>Buy</button>  
+                <form onSubmit={this.handleSubmit}>
+                    
+                    <div class="form-group my-4 ml-5 w-50">
+                        <input name="ticker" type="text" class="form-control" placeholder="Ticker" value={this.state.ticker} onChange={this.handleChange} required/>
+                    </div>
+                    
+                    <div class="form-group ml-5 w-50">
+                        <input name="qty" type="number" class="form-control" placeholder="Qty" value={this.state.qty} onChange={this.handleChange} required/>
+                    </div>
+                    
+                    <button type="submit" class="btn btn-primary">Buy</button> 
+                </form>
+                
             </div>
         )
     }
